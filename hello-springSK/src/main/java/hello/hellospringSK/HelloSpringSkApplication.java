cce7af5e3c24a9cf7b6cbacf073f3f220712660a
yeom <duawofud@nate.com>
2023-01-03 21:34:19 +0900

    SK -----------

---
+++ hello-springSK/src/main/java/hello/hellospringSK/HelloSpringSkApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospringSK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringSkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringSkApplication.class, args);
+	}
+
+}
---
+++ hello-springSK/src/test/java/hello/hellospringSK/HelloSpringSkApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospringSK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringSkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
